5a9df305f877a6e9d44626af28d0011aad98b3d5
Olivier <ol.maignan@gmail.com>
2024-04-03 17:02:58 +0200

    creation d'un footer basic bootstrap

---
+++ src/Views/Includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-body-tertiary text-center">
+    <div class="container p-4">
+        <p>&copy; Copyright <?php echo date('Y'); ?> Music Vercors Festival. Tous droits réservés.</p>
+    </div>
+</footer>
